d54b08da6d6be37f2f1cde1330193ee3e5a16e64
OwenCClark <34281239+OwenCClark@users.noreply.github.com>
2019-12-10 23:14:28 -0500

    initial commit

---
+++ chem.js
@@ -0,0 +1,43 @@
+var chem = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 36.4849,35.6599 ]
+    },
+    "properties": {
+    "data_id":6081850,
+    "iso":760,
+    "event_id_cnty":"SYR61160",
+    "event_id_no_cnty":61160,
+    "event_date":"24 May 2019",
+    "year":2019,
+    "time_precision":"1",
+    "event_type":"Explosions/Remote violence",
+    "sub_event_type":"Chemical weapon",
+    "actor1":"Military Forces of Syria (2000-)",
+    "assoc_actor_1":"Allied Syrian and/or Russian Forces",
+    "inter1":"1",
+    "actor2":"",
+    "assoc_actor_2":"",
+    "inter2":"0",
+    "interaction":10,
+    "region":"Middle East",
+    "country":"Syria",
+    "admin1":"Idleb",
+    "admin2":"Ariha",
+    "admin3":"Ehsem",
+    "location":"Kansafra",
+    "geo_precision":"1",
+    "source":"Liveuamap",
+    "source_scale":"Local partner-New media",
+    "notes":"On 24 May 2019, regime ground forces fired artillery and Syrian and/or Russian warplanes carried out air raids that produced a yellow cloud that appeared to be chlorine on the town of Kansafra. Neither injuries nor fatalities reported.",
+    "fatalities":"0",
+    "timestamp":1571164584,
+    "iso3":"SYR"
+    }
+  }
+]
+}
